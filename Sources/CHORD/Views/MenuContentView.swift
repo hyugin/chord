ad7a21b33a4ca28abb5ae99abfe8938d6fe4b724
hugivar <red.clock0044@fastmail.com>
2026-07-23 15:16:50 -0400

    fix(menu): simplify Check for Updates label

--- Sources/CHORD/Views/MenuContentView.swift
+++ Sources/CHORD/Views/MenuContentView.swift
@@ -32,7 +32,7 @@ struct MenuContentView: View {
           windowManager.openKeyboardMap(appState: appState, monitor: monitor)
         }
 
-        MenuActionRow(title: "Check for Updates…") {
+        MenuActionRow(title: "Check for Updates") {
           Task {
             await DownloadsUpdateInstaller.installFromDownloadsInteractively()
           }
